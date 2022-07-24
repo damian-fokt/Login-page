e3439975462441962fce34e96654d7f048c2cf9a
Damian <damian.fokt@op.pl>
2022-07-24 21:47:54 +0200

    Adding conteiners etc.

--- index.php
+++ index.php
@@ -8,5 +8,29 @@ get_header();
 echo esc_html( get_template_part( 'template-parts/pagination', 'infinite-scroll' ) );
 ?>
 
+<section class="loginpage-background">
+		<div class="loginpage-form">
+			<h1 class="loginpage-form__header">
+				Log in
+			</h1>
+			<p class="loginpage-form__description">
+				Log in with your data that you received from the photographer
+			</p>
+			<form>
+				<label>
+					<input class="loginpage-form__input" type="email" placeholder="E-mail">
+				</label>
+				<label>
+					<input class="loginpage-form__input" type="password" placeholder="Password">
+				</label>
+				<div class="loginpage-btn">
+					<a class="loginpage-btn__link" href="#">Forgot password?</a>
+					<input class="loginpage-btn__submit" type="submit" value="Log in">
+				</div>
+			</form>
+		</div>
+	</section>
+
 <?php
 get_footer();
+?>
